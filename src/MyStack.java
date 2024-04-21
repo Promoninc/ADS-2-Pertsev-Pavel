606f1a4c0bd08eed969e788606055f3208999d64
Promoninc <promoninc@gmail.com>
2024-04-21 22:13:36 +0500

    Added new class MyStack

---
+++ src/MyStack.java
@@ -0,0 +1,28 @@
+public class MyStack<T>{
+    private MyLinkedList<T> list = new MyLinkedList<>(); //The main list that contains all elements of stack
+
+    //Function checks is the stack empty
+    public boolean isEmpty(){
+        return list.size() != 0;
+    }
+
+    //Function return number of elements in stack
+    public int size(){
+        return list.size();
+    }
+
+    //Function returns last element of stack
+    public T peek(){
+        return list.getLast();
+    }
+
+    //Function adds new element to the end of stack
+    public void push(T item){
+        list.addLast(item);
+    }
+
+    //Function deletes last element in stack
+    public void pop(){
+        list.removeLast();
+    }
+}
